be7158deda5b46e7c9a552e750f53f26a15059d1
Erik G. Brandt <erik.brandt@shaarpec.com>
2023-01-04 15:49:53 +0100

    ✨ Add task running to client

--- shaarpec/client.py
+++ shaarpec/client.py
@@ -1,11 +1,47 @@
 """Client for SHAARPEC Analytics API."""
 from __future__ import annotations
-from typing import Union
+from typing import Optional, Union, Any
+import pathlib
+import time
 
+from pydantic import BaseModel
+from tqdm.notebook import tqdm
+import background
 import httpx
 
 from oidcish.device import DeviceFlow
 from oidcish.code import CodeFlow, CodeSettings
+
+
+class Task(BaseModel):
+    """A running task."""
+
+    service: str
+    task_id: str
+    submitted_at: str
+    status: str
+    success: Optional[bool]
+    progress: Optional[float]
+    result: Optional[Any]
+    error: Optional[Any]
+
+    @background.task
+    def print(self, update_interval: float = 0.1) -> None:
+        """Print the progress of the task."""
+        initial = 0 if self.progress is None else int(100 * self.progress)
+
+        with tqdm(initial=initial, total=100) as pbar:
+            pbar.set_description(f"Task {self.status}")
+
+            while self.status in ("submitted", "queued", "in_progress"):
+                fraction = 0 if self.progress is None else self.progress
+                pbar.update(int(100 * fraction) - pbar.n)
+                pbar.set_description(f"Task {self.status}")
+                time.sleep(update_interval)
+
+            pbar.set_description(
+                f"Task completed ({'successfully' if self.success else 'failed'})"
+            )
 
 
 class Client:
@@ -211,3 +247,101 @@ class Client:
 
         return response
 
+    def run(
+        self,
+        uri: str,
+        content=None,
+        data=None,
+        files=None,
+        json=None,
+        poll_interval: float = 0.1,
+        progress_bar: bool = True,
+        **kwargs,
+    ) -> Task:
+        """Run a task at resource `uri`.
+
+        A task is equivalent to posting and polling until the job is finished.
+
+        Keyword arguments are passed as query parameters.
+        """
+        service = pathlib.Path(uri).parts[0]
+
+        response = self.post(
+            uri=uri, content=content, data=data, files=files, json=json, **kwargs
+        )
+
+        task_id = response.json().get("task_id")
+        submitted_at = response.json().get("submitted_at")
+
+        task = Task(
+            service=service,
+            task_id=task_id,
+            submitted_at=submitted_at,
+            status="submitted",
+            success=None,
+            progress=None,
+            result=None,
+            error=None,
+        )
+
+        if progress_bar:
+            task.print()
+
+        self._wait_for_task(task, poll_interval=poll_interval)
+
+        return task
+
+    @background.task
+    def _wait_for_task(self, task: Task, poll_interval: float = 0.1) -> None:
+        while task.status in ("submitted", "queued", "in_progress"):
+            response = self.get(f"{task.service}/tasks/{task.task_id}/status")
+
+            match response.status_code:
+                case 401:
+                    print(
+                        f"Not authorized to run task id {task.task_id} on service {task.service}."
+                    )
+                    task.success = False
+                    task.status = "unauthorized"
+
+                case 404:
+                    print(
+                        f"Task with id {task.task_id} could not be found on service {task.service}."
+                    )
+                    task.success = False
+                    task.status = "not_found"
+
+                case 200:
+                    match response.json():
+                        case {"status": "not_found"}:
+                            print(
+                                f"Task with id {task.task_id} "
+                                f"could not be found on service {task.service}."
+                            )
+                            task.status = "not_found"
+
+                        case {"status": "in_progress" | "queued"} as data:
+                            task.progress = data.get("progress")
+                            task.status = data.get("status")
+                            time.sleep(poll_interval)
+
+                        case {"status": "complete", "success": True} as data:
+                            resp = self.get(
+                                f"{task.service}/tasks/{task.task_id}/results"
+                            )
+
+                            task.result = resp.json().get("result")
+                            task.progress = 1.0
+                            task.success = True
+                            task.status = "complete"
+
+                        case _ as data:
+                            task.error = data.get("error")
+                            task.success = False
+                            task.status = "complete"
+
+                case _:
+                    print(f"An error occurred on service {task.service}.")
+                    task.error = response.json().get("error")
+                    task.success = False
+                    task.status = "error"
